1eee25526c1a721ee3178b9155d4233bb58355ff
Luis Campos <lucam75@gmail.com>
2021-06-12 11:03:23 -0400

    Datatypes

---
+++ datatype.py
@@ -0,0 +1,26 @@
+#String
+print("Hello world")
+
+#Integer
+print(10)
+# Float
+print(10.1)
+print(20.5)
+
+# Boolean
+print(True)
+print(False)
+
+# List
+print([10, 20, 30, 40])
+print([10, "Hello", True, 10.1])
+
+# Tuples
+print((10, 20, 30, 40))
+
+#Dictionaries
+print({
+    "name": "Ryan",
+    "lastname": "Reynolds",
+    "nickname": "Deadpool"
+})
